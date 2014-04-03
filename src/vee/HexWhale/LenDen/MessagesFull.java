c6e4045fa7cc81c2b8dceea19f8a844c36b3e93c
VenomVendor <info@VenomVendor.com>
2014-04-04 00:32:46 +0530

    * New Branch Commit Test
    Signed-off-by: VenomVendor <info@VenomVendor.com>

--- src/vee/HexWhale/LenDen/MessagesFull.java
+++ src/vee/HexWhale/LenDen/MessagesFull.java
@@ -1,15 +1,15 @@
 /**
- * ***Copyright(c)	:	2014-Present, VenomVendor.***
- * Author			:	VenomVendor
- * Dated			:	21 Feb, 2014 4:41:51 PM
- * Project			:	LenDen-Android
- * Client			:	LenDen
- * Contact			:	info@VenomVendor.com
- * URL				:	https://www.google.co.in/search?q=VenomVendor
- * Copyright(c)		:	2014-Present, VenomVendor.
- * License			:	This work is licensed under Attribution-NonCommercial 3.0 Unported
- *						License info at http://creativecommons.org/licenses/by-nc/3.0/deed.en_US
- *						Read More at http://creativecommons.org/licenses/by-nc/3.0/legalcode
+ * ***Copyright(c)		:	2014-Present, VenomVendor.***
+ * Author				:	VenomVendor
+ * Dated				:	21 Feb, 2014 4:41:51 PM
+ * Project				:	LenDen-Android
+ * Client				:	LenDen
+ * Contact				:	info@VenomVendor.com
+ * URL					:	https://www.google.co.in/search?q=VenomVendor
+ * Copyright(c)			:	2014-Present, VenomVendor.
+ * License				:	This work is licensed under Attribution-NonCommercial 3.0 Unported
+ *							License info at http://creativecommons.org/licenses/by-nc/3.0/deed.en_US
+ *							Read More at http://creativecommons.org/licenses/by-nc/3.0/legalcode
  **/
 
 package vee.HexWhale.LenDen;
